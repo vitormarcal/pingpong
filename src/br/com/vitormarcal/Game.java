242fc7782f2ced67c6ee59188764de64b0064c40
Vítor Marçal <git@vitormarcal.com.br>
2019-10-10 20:55:42 -0300

    criando colisoes e movimentos esquerda e direita

--- src/br/com/vitormarcal/Game.java
+++ src/br/com/vitormarcal/Game.java
@@ -2,10 +2,12 @@ package br.com.vitormarcal;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
     public static int WIDTH = 240;
     public static int HEIGHT = 120;
@@ -16,7 +18,8 @@ public class Game extends Canvas implements Runnable {
 
     public Game() {
         this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-        player = new Player();
+        this.addKeyListener(this);
+        player = new Player(100, HEIGHT -10);
         layer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
     }
 
@@ -36,7 +39,7 @@ public class Game extends Canvas implements Runnable {
     }
 
     public void tick() {
-
+        player.tick();
     }
 
     public void render() {
@@ -46,6 +49,8 @@ public class Game extends Canvas implements Runnable {
             bufferStrategy = this.getBufferStrategy();
         }
         Graphics graphics = layer.getGraphics();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0, WIDTH, HEIGHT);
         player.render(graphics);
 
 
@@ -64,6 +69,29 @@ public class Game extends Canvas implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            player.right = true;
+        } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            player.left = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            player.right = false;
+        } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+            player.left = false;
         }
     }
 }
--- src/br/com/vitormarcal/Player.java
+++ src/br/com/vitormarcal/Player.java
@@ -4,13 +4,41 @@ import java.awt.*;
 
 public class Player {
 
+
+    private final int height;
+    public boolean right;
+    public boolean left;
+    public int x;
+    public int y;
+    private int width;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+        width = 40;
+        height = 10;
+    }
+
     public void tick() {
+
+        if (right) {
+            x++;
+        } else if (left) {
+            x--;
+        }
+
+
+        if (x+width > Game.WIDTH) {
+            x = Game.WIDTH - width;
+        } else if (x < 0) {
+            x = 0;
+        }
 
     }
 
     public void render(Graphics graphics) {
         graphics.setColor(Color.BLUE);
-        graphics.fillRect(100, 120-10, 40,10);
+        graphics.fillRect(x, y, width, height);
     }
 
 }
